8b8cf63ccbddaec9424248a4bdd7aa377a61b4fe
Bokseunghyun <53068378+Bokseunghyun@users.noreply.github.com>
2026-01-25 04:50:28 +0900

    e

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
+  server: {
+    proxy: process.env.NODE_ENV === 'development' ? {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    } : {}
   }
 });
+
