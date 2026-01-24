04ed2836bf375ec2a800463d0ebc61323376b38f
Bokseunghyun <53068378+Bokseunghyun@users.noreply.github.com>
2026-01-25 00:07:37 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // 배포 시 경로 문제 방지
+});
