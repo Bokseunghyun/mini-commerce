3ac2457d5752a7e96e6b4bdf44ea379b38ec54cf
Bokseunghyun <53068378+Bokseunghyun@users.noreply.github.com>
2026-01-25 05:47:21 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './', // 배포 시 상대경로 처리
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000', 
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '/api'),
-      },
-    },
-  },
+ 
 });
